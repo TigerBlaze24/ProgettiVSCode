0ee1307c72af9589de1351eac4f09783d018755c
TigerBlaze24 <cuanbellina@gmail.com>
2024-05-07 13:50:47 +0200

    esercizi

--- Esercizi/Lezione2/lezione6.py/esercizi6.py
+++ Esercizi/Lezione2/lezione6.py/esercizi6.py
@@ -17,3 +17,33 @@ print( f'il nome della persona piu giovane{[index_min_age]}')
 #facendo la funzione def__str__(self)->str:
 #return f'Person(name={self.name})'
 #si evita di printare tutte le classi ogni volta
+class student:
+  def __init__(self,name: str, studyprogram:str,age:str,gender:str):
+       self.name = name
+       self.studyrprgram = studyprogram
+       self.age= age
+       self.gender = gender
+def __str__(self)->str:
+         return f'student(name={self.name}, age ={self.age}, gender={self.gender})'
+studenti =[ student("Filippo","medicina",18,"M"),student("Alina","matematica",20,"F"),student("Mike", "fisica",40,"M")]
+io=student("Filippo","medicina",18,"M")
+filippo=student("Alina","matematica",20,"F")
+mike =student("Mike", "fisica",40,"M")
+for studente in studenti:
+    print(studente.name)
+    print(studente.studyrprgram)
+    print(studente.age)
+    print(studente.gender)
+#################################################################################################
+class animal:
+  def __init__(self,name: str, legs:int):
+       self.name = name
+       self.legs = legs
+  def get_legs(self) -> int:
+      return self.legs
+cane = animal("cane",4)
+print(cane.get_legs())
+cane.get_legs(-3)
+print(cane.get_legs)
+      
+
