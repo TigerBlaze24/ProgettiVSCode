1e00d818d98af11296052f0a582b14857432dae2
TigerBlaze24 <cuanbellina@gmail.com>
2024-05-08 11:42:25 +0200

    esercizi6(classi).py

--- Esercizi/Lezione2/lezione6.py/esercizi6(classi).py
+++ Esercizi/Lezione2/lezione6.py/esercizi6(classi).py
@@ -43,7 +43,38 @@ class animal:
       return self.legs
 cane = animal("cane",4)
 print(cane.get_legs())
-cane.get_legs(-3)
-print(cane.get_legs)
+
+ ##########################################################################  
+    
+class food:
+    def __init__(self,name: str, price:str,description:str):
+        self.name = name
+        self.price = price
+        self.description= description
+    def __str__(self)->str:
+        return f'(name={self.name},price={self.price},description={self.description})'
+piatti  =[food("lasagna",8,"pasta"),food("cannelloni",5,"pasta"),food("pollo alla diavola", 10,"pollo")]
+class menu:
+    foods=[]
+    def addfood(self,food:list[food] =[]):
+        self.foods.append(food)
+    def __removefood__(self,food:list[food] =[]): 
+        self.foods.append(food)
+    def __str__(self)-> str:
+        repr:str =""
+        for food in self.foods:
+            repr += food.__str__() +"\n"
+        return repr[:-1]
+cimbellone=food(name="ciambellone",price=10,description="dolce a forma di anello")
+tiramisu=food(name="tiramisu",price=8,description="dolce al mascarpone e caffe")
+budino=food(name="budino",price=5,description="dolce al cucchiaio")
+
+menu = menu()
+menu.addfood(cimbellone)
+menu.addfood(tiramisu)
+menu.addfood(budino)
+print (menu)
+
       
 
+       
