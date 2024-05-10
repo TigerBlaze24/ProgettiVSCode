8dab4283331b09ae8c67840ba925a813fe96ace5
Nio4124 <antoniozottola41204@gmail.com>
2024-05-10 14:02:17 +0200

    esercizi6(classi).py

--- Esercizi/Lezione2/lezione6.py/esercizi6(classi).py
+++ Esercizi/Lezione2/lezione6.py/esercizi6(classi).py
@@ -75,6 +75,60 @@ menu.addfood(tiramisu)
 menu.addfood(budino)
 print (menu)
 
-      
+   #1Scrivi una funzione che verifica se una combinazione di condizioni (A, B, e C) è soddisfatta per procedere con un'operazione. L'operazione può procedere solo se la condizione A è vera o se entrambe le condizioni B e C sono vere. La funzione deve ritornare "Operazione permessa" oppure "Operazione negata" a seconda delle condizioni che sono sodd#
+def check_combination(conditionA: bool, conditionB: bool, conditionC: bool) -> str:
+    
+    if conditionA or (conditionB and conditionC):
+        return "Operazione permessa"
+    else:
+        return "Operazione negata"
 
-       
+
+print(check_combination(True, False, False))
+print(check_combination(False, True, True))  
+print(check_combination(False, False, True)) 
+
+def remove_duplicates() -> list:
+    unic = []
+    for element in list:
+        if element not in unic:
+            unic.append(element)
+    return unic
+
+
+'''
+esercio 2 Scrivi una funzione che rimuove tutti i duplicati da una lista, contenente sia numeri che lettere, mantenendo l'ordine originale degli elementi.
+def rimuovi_elementi(lista, dizionario):
+    for elemento, frequenza in dizionario.items():
+        for _ in range(frequenza):
+            if elemento in lista:
+                lista.remove(elemento)
+    return lista
+'''
+
+#esercizio 3 Scrivi una funzione che elimini dalla lista dati certi elementi specificati in un dizionario. Il dizionario contiene elementi da rimuovere come chiavi e il numero di volte che devono essere rimossi come valori.#
+def rimuovi_elementi(lista: list[int], da_rimuovere: dict[int:int]) -> list[int]:
+    for chiave, valore in da_rimuovere.items():
+        while valore in lista:  
+            lista.remove(valore)
+    return lista
+
+
+lista_dati = [1, 2, 3,2,4]
+dizionario_da_rimuovere = {2: 2, 4: 1}
+nuova_lista = rimuovi_elementi(lista_dati, dizionario_da_rimuovere)
+
+ #esercizio 4 ripassoScrivi una funzione che prenda in input una lista di dizionari che rappresentano voti di studenti e aggrega i voti per studente in un nuovo dizionario.#
+def aggrega_voti(voti: dict) -> dict[str:list[int]]:
+    
+    aggrega_vot = {}
+    
+    for studente in voti:
+        nome = studente["nome"]
+        voto = studente['voto']
+        if  nome in aggrega_vot:
+            aggrega_vot[nome].append(voto)
+        else:
+            aggrega_vot[nome] = [voto]
+    return aggrega_vot
+print(aggrega_voti([{'nome': 'Alice', 'voto': 90}, {'nome': 'Bob', 'voto': 75}, {'nome': 'Alice', 'voto': 85}]))
