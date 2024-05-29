864e87c93a47f6b1b495aaaaba553b486627927e
Nio4124 <antoniozottola41204@gmail.com>
2024-05-29 15:00:59 +0200

    upgrade

--- Esercizi/Lezione2/lezione6.py/lalepreelatartaruga.py
+++ Esercizi/Lezione2/lezione6.py/lalepreelatartaruga.py
@@ -1,39 +1,50 @@
-import random
-def visualizza_posizioni(pos_tartaruga:str, pos_lepre:str) ->int:   
-    corsa_animali :list = []
-    for i in range (71):    
-        corsa_animali.append("_")
-    print(corsa_animali)
-def movimento_tartaruga(passo_veloce,scivolata,passo_lento)->int:
-    i = random.randint(0,10)
-    if i in range (1,5):
-        mossa = 3 
-        return mossa
-    if i in range (6,7):
-      mossa = -6
-      return mossa
-    if i in range(8,9,10):
-        mossa = 1 
-        return mossa
-def movimento_lepre(riposo,grande_balzo,grande_scivolata,piccolo_balzo,piccola_scivolata)->int:
-    i = random.randint(0,10)
-    if i in range(1,2):
-        mossa = 0
-    if i in range(1,2):
-        mossa = 9
-    if i in range (1,1):    
-        mossa = -12
-    if i in range (1,3):
-        mossa = 1
-    if i in range(1,2): 
-        mossa = -2
-print("'BANG !!!!! AND THEY'RE OFF !!!!!'")
-tartaruga = 1
-tartaruga = movimento_tartaruga()   
-lepre = 1
-lepre = movimento_lepre()
+import random   
+def movimento_tartaruga():
+    i = random.randint(1, 10)
+    if 1 <= i <= 5:
+        return 3
+    elif 6 <= i <= 7:
+        return -6
+    else:
+        return 1
+def movimento_lepre():
+    i = random.randint(1, 10)
+    if 1 <= i <= 2:
+        return 0
+    elif 3 <= i <= 4:
+        return 9
+    elif i == 5:
+        return -12
+    elif 6 <= i <= 8:
+        return 1
+    else:
+        return -2
 
-
-
-
-    
+def visualizza_posizioni(tartaruga, lepre):
+    for i in range(1, 71):
+        if i == tartaruga == lepre:
+            print('OUCH!!!', end='')
+        elif i == tartaruga:
+            print('T', end='')
+        elif i == lepre:
+            print('H', end='')
+        else:
+            print('_', end='')
+    print()
+def gara():
+    tartaruga = lepre = 1
+    print("BANG !!!!! AND THEY'RE OFF !!!!!")
+    while True:
+        tartaruga = max(1, tartaruga + movimento_tartaruga())
+        lepre = max(1, lepre + movimento_lepre())
+        visualizza_posizioni(tartaruga, lepre)
+        if tartaruga >= 70 and lepre >= 70:
+            print("IT'S A TIE.")
+            break
+        elif tartaruga >= 70:
+            print("TORTOISE WINS! || VAY!!!")
+            break
+        elif lepre >= 70:
+            print("HARE WINS || YUCH!!!")
+            break
+gara()
