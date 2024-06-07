d7fba8cc7184cdab8de66550c7bea02df07fd9c4
TigerBlaze24 <cuanbellina@gmail.com>
2024-06-07 17:53:00 +0200

    aggiunto esercizio

--- Esercizi/Lezione2/esercizi in classe.py
+++ Esercizi/Lezione2/esercizi in classe.py
@@ -62,9 +62,57 @@ linked_list.append(2)
 linked_list.append(1)
 print(linked_list.is_palindrome())  # Output: True
 #####################################################################
-class ContextManager:
+"""Obiettivo:
+Implementare una classe Media che rappresenti un media generico (ad esempio, un film o un libro) e una classe derivata Film che rappresenti specificamente un film. Gli studenti dovranno anche creare oggetti di queste classi, aggiungere valutazioni e visualizzare le recensioni.
 
+Specifiche della Classe Media:
+ 
+Attributi:
+- title (stringa): Il titolo del media.
+- reviews (lista di interi): Una lista di valutazioni del media, con voti compresi tra 1 e 5, dove 1=Terribile, 2=Brutto, 3=Normale, 4=Bello, 5=Grandioso.
 
-
+Metodi:
+- C Imposta il titolo del media.
+- get_title(self): Restituisce il titolo del media.
+- aggiungiValutazione(self, voto): Aggiunge una valutazione alla lista delle recensioni se è valida (tra 1 e 5).
+- getMedia(self): Calcola e restituisce la media delle valutazioni.
+- getRate(self): Restituisce una stringa che descrive il giudizio medio basato sulla media delle valutazioni, ovvero mostra "Terribile" se il voto medio si avvicina a 1, "Brutto" se il voto medio si avvicina a 2, "Normale" se il voto medio si avvicina a 3, "Bello" se il voto medio si avvicina a 4, "Grandioso" se il voto medio si avvicina a 5.
+- ratePercentage(self, voto): Calcola e restituisce la percentuale di un voto specifico nelle recensioni.
+- recensione(self): Mostra un riassunto delle recensioni e delle valutazioni del media, stampando il titolo, il voto medio, il giudizio e le percentuali di ciascun voto.
+"""
+class Media:
+ def __init__ ( self,tile:str,reviews:list,media:int,voto:int):
+    self.title = str
+    self.reviews = list
+    self.voto= int
+    self.media = int
+ def set_title(self,imposttitle):
+    self.title= imposttitle
+    return imposttitle
+def get_title(self,gettitle):
+    self.gettitle=gettitle
+    return gettitle
+def getmedia(self,media):
+    self.media= media
+    return media
+def aggiungi_valutazione(self,voto):
+    self.aggiungivalutazione =voto
+    return voto
+def get_rate(self,rate):
+    if rate==1:
+     return"terribile"
+    elif rate==2:
+     return"normale"
+    elif rate==3:
+     return"normale"
+    elif rate==4:
+     return"bello"
+    elif rate==5:   
+       return"grandioso"       
+def percentage(self,voto :int):
+#da finire a casa
+   
+   
+     
 
 
